5f24835d742f82afb714f9b9acd252d9f8003b5e
[Anthony] <stine.anthony@yahoo.com>
2023-01-12 19:12:41 -0500

    added a birthday button

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,7 +1,11 @@
 import React from "react"
+import { useState } from "react";
 
 const PersonCard = (props) => {
-    const { firstName, lastName, age, hairColor } = props;
+    const [ age, setAge ] = useState(props.age);
+    //useState is a function when you call it is an array. The first element will be a piece of "state", the second will upate the piece of state
+    // Optional: initial value. not necessary, but good practice
+    const { firstName, lastName, hairColor } = props;
 
     return (
         <div className='card mb-2'>
@@ -9,7 +13,13 @@ const PersonCard = (props) => {
             <div className="card-body">
                 <p className='card-text'>Age: {age}</p>
                 <p className='card-text'>Hair Color : {hairColor}</p>
-            </div>    
+            </div>
+            <button 
+            // anfn onclick button shortcut
+            onClick={() => setAge(age + 1)}
+            className="btn btn-secondary">
+                Birthday
+            </button>    
         </div>
     )
 }
